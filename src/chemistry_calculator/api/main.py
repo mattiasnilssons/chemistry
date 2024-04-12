6e00d8f9bc43205c69d625092f8826d8b314ebde
mn-thermodynamics <mn@heaten.com>
2024-04-13 01:18:46 +0200

    Simplify the main file

--- src/chemistry_calculator/api/main.py
+++ src/chemistry_calculator/api/main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI, APIRouter
 from chemistry_calculator.mould_temperature import get_mould_data
-import uvicorn
 import os
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
 from dotenv import load_dotenv
+
+app = FastAPI()
 load_dotenv()  # This loads the .env file
+
+# Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("VITE_WEBSITE_HOST"),  # Adjust the port if your Svelte app runs on a different one
+    allow_origins=os.getenv("VITE_WEBSITE_HOST"),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Set up the API router
 router = APIRouter()
 
 @router.post("/calculate-mould")
@@ -21,6 +25,7 @@ async def calculate_mould():
     return results
 
 app.include_router(router)
-portname = int(os.getenv("APP_PORT"))
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=portname, reload=True)
+# portname = int(os.getenv("APP_PORT"))
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=portname, reload=True)
