2040b626ef68deb338164ad6797614d1617f5c73
mn-thermodynamics <mn@heaten.com>
2024-04-13 02:39:27 +0200

    Change node version back

--- src/chemistry_calculator_portal/vite.config.ts
+++ src/chemistry_calculator_portal/vite.config.ts
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfig = {
+  plugins: [sveltekit()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "src/custom.scss" as *;'
+      }
+    }
+  }
+};
+
+export default config;
