19d21ac03af4e0222c878b7f82d39888882b727b
mn-thermodynamics <mn@heaten.com>
2024-04-14 09:43:29 +0200

    Update the mold diagram

--- src/chemistry_calculator_portal/src/component/MouldGraph.svelte
+++ src/chemistry_calculator_portal/src/component/MouldGraph.svelte
@@ -1,11 +1,47 @@
 <script>
   import { writable } from "svelte/store";
   import Chart from 'chart.js/auto';
+  import Trans from "../library/Trans.svelte";
 
   let chart;
   let chartContainer;
   let climateData = writable({});
-  let riskFactor = writable(20); // Default value for the slider
+  let riskFactor = writable(75); // Default value for the slider
+
+  const humidityLevels = {
+    'Låg': {
+      description: 'Under 60% RH: Idealisk för att förhindra alla former av mögeltillväxt.',
+      risk: 'Låg',
+      baseRisk: 60,
+      molds: 'De flesta mögelformer kan inte etablera sig under dessa torra förhållanden.'
+    },
+    'Måttlig': {
+      description: '60-70% RH: Vissa mögelsorter som Aspergillus och Penicillium börjar trivas, speciellt i varmare miljöer.',
+      risk: 'Måttlig',
+      baseRisk: 65,
+      molds: 'Aspergillus och Penicillium trivs i denna fuktighetsnivå.'
+    },
+    'Hög': {
+      description: '70-80% RH: Mögelrisken ökar, och arter som Cladosporium och Stachybotrys (svartmögel) blir mer aktiva.',
+      risk: 'Hög',
+      baseRisk: 75,
+      molds: 'Cladosporium och Stachybotrys (Svartmögel) blir alltmer vanliga.'
+    },
+    'Mycket hög': {
+      description: 'Över 80% RH: Höga fuktighetsnivåer stödjer tillväxten av nästan alla mögelformer, särskilt i dåligt ventilerade områden.',
+      risk: 'Mycket hög',
+      baseRisk: 80,
+      molds: 'Nästan alla mögelarter, inklusive de som är hälsoskadliga, kan växa ostört.'
+    }
+  };
+
+  let selectedHumidityLevel = writable('');
+
+  function handleHumiditySelection(event) {
+    selectedHumidityLevel.set(event.target.value);
+    riskFactor.set(humidityLevels[event.target.value].baseRisk);
+    updateChart();
+  }
 
   async function fetchMoldData() {
     const response = await fetch('http://localhost:8080/calculate-mould', {
@@ -22,12 +58,12 @@
   }
 
   function calculateMoldRisk(RH, temperature, factor) {
-    return factor + RH + 0.5 * temperature; // Use factor from the slider
+    return factor + 20 * Math.exp(-0.1241 * temperature);
   }
 
   function updateChart() {
-    const data = $climateData; // Using the store's auto-subscription feature
-    const factor = $riskFactor; // Dynamic risk factor from the slider
+    const data = $climateData;
+    const factor = $riskFactor;
 
     if (data && data.temperatures && data.humidity) {
       const temperatures = Object.values(data.temperatures);
@@ -73,9 +109,33 @@
 
 <div class="container">
   <div class="main-content">
-    <button on:click={fetchMoldData}>Fetch Mold Data</button>
-    <input type="range" min="10" max="30" value={$riskFactor} on:input={(e) => riskFactor.set(+e.target.value)}>
-    <p>Risk Factor: {$riskFactor}</p>
+    <div class="row">
+      <div class="col-6">
+    <h2><Trans text="Välj relativ fuktighetsnivå"/></h2>
+        <div class="mb-4">
+    {#each Object.keys(humidityLevels) as level}
+      <label class="form-check-label">
+        <input type="radio" name="humidity" class="form-check-input" value={level} on:change={handleHumiditySelection}>
+        {level}
+      </label>
+    {/each}
+          </div>
+        <div class="mb-4">
+            <button class="btn btn-outline-primary" on:click={fetchMoldData}>Hämta mögeldata</button>
+    <input type="range" min="10" max="90" value={$riskFactor} on:input={(e) => riskFactor.set(+e.target.value)}>
+    <div class="lead">Riskfaktor: {$riskFactor}</div>
+          </div>
+        </div>
+      <div class="col-6">
+    {#if $selectedHumidityLevel}
+      <h2>Om {$selectedHumidityLevel.toLowerCase()} fuktighetsnivå</h2>
+      <div class="form-text"><b>Beskrivning:</b> {humidityLevels[$selectedHumidityLevel].description}</div>
+      <div class="form-text"><b>Risk för mögel:</b> {humidityLevels[$selectedHumidityLevel].risk}</div>
+      <div class="form-text"><b>Favoriserade mögelsorter:</b> {humidityLevels[$selectedHumidityLevel].molds}</div>
+    {/if}
+        </div>
+      </div>
+
     <canvas bind:this={chartContainer}></canvas>
   </div>
 </div>
--- src/chemistry_calculator_portal/src/component/OverviewGraph.svelte
+++ src/chemistry_calculator_portal/src/component/OverviewGraph.svelte
@@ -1,30 +1,33 @@
 <script>
-
   import Chart from 'chart.js/auto';
-  import {get, writable} from "svelte/store";
+  import { writable, get } from "svelte/store";
+  import annotationPlugin from 'chartjs-plugin-annotation'; // Ensure you've installed this
+  import 'chartjs-adapter-date-fns'; // Import the adapter
+
+  Chart.register(annotationPlugin); // Register the annotation plugin globally
 
   let chart;
   let chartContainer;
   let climateData = writable({});
 
+  const events = [
+    { time: '2019-03-26T12:00', description: 'I klassrummet (labbsalen) ligger på bänken' },
+    { time: '2019-03-26T19:00', description: 'Transport (i väska)' },
+    // Add all other events following the same format
+    { time: '2019-04-11T09:55', description: 'Ställd i klassrummet' }
+  ];
+
   async function fetchMoldData() {
-    try {
-      const response = await fetch('http://localhost:8080/calculate-mould', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Data", data)
-        climateData.set(data);
-        updateChart();
-      } else {
-        throw new Error('Failed to fetch');
-      }
-    } catch (error) {
-      console.error('Failed to fetch data:', error);
+    const response = await fetch('http://localhost:8080/calculate-mould', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      climateData.set(data);
+      updateChart();
+    } else {
+      console.error('Failed to fetch data:', await response.text());
     }
   }
 
@@ -33,59 +36,84 @@
   }
 
   function updateChart() {
-    const data = get(climateData); // Assuming you store fetched data into a Svelte store
-    console.log(data); // Check the structure here
-
-    if (!data || typeof data !== 'object') {
-        console.error('Invalid or no data available');
-        return;
+    const data = get(climateData);
+    if (!data || !data.temperatures || !data.humidity) {
+      console.error('Invalid or no data available');
+      return;
     }
 
-    // Assuming data is structured with these specific object keys
-    try {
-        const dates = data.dates ? Object.values(data.dates) : [];
-        const temperatures = data.temperatures ? Object.values(data['temperatures']) : []; // Example of accessing the °C data
-        const humidity = data.humidity ? Object.values(data['humidity']) : []; // Accessing the %RH data
+    const dates = Object.keys(data.temperatures);
+    const temperatures = Object.values(data.temperatures);
+    const humidity = Object.values(data.humidity);
+    const moldRisks = humidity.map((rh, index) => calculateMoldRisk(rh, temperatures[index]));
 
-        const moldRisks = humidity.map((rh, index) => calculateMoldRisk(rh, temperatures[index]));
+    const eventAnnotations = events.map(event => ({
+      type: 'line',
+      mode: 'vertical',
+      scaleID: 'x',
+      value: event.time,
+      borderColor: 'orange',
+      borderWidth: 2,
+      label: {
+        enabled: true,
+        content: event.description,
+        backgroundColor: 'rgba(255, 204, 0, 0.8)',
+        position: "top"
+      }
+    }));
 
-        if (chart) {
-            chart.data.labels = dates;
-            chart.data.datasets[0].data = temperatures;
-            chart.data.datasets[1].data = moldRisks;
-            chart.update();
-        } else {
-            chart = new Chart(chartContainer, {
-                type: 'line',
-                data: {
-                    labels: dates,
-                    datasets: [{
-                        label: 'Temperature (°C)',
-                        data: temperatures,
-                        borderColor: 'blue'
-                    }, {
-                        label: 'Mold Risk',
-                        data: moldRisks,
-                        borderColor: 'red'
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+    if (chart) {
+      chart.data.labels = dates;
+      chart.data.datasets[0].data = temperatures;
+      chart.data.datasets[1].data = moldRisks;
+      chart.options.plugins.annotation.annotations = eventAnnotations;
+      chart.update();
+    } else {
+      chart = new Chart(chartContainer, {
+        type: 'line',
+        data: {
+          labels: dates,
+          datasets: [{
+            label: 'Temperature (°C)',
+            data: temperatures,
+            borderColor: 'blue'
+          }, {
+            label: 'Mold Risk',
+            data: moldRisks,
+            borderColor: 'red'
+          }]
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                parser: 'yyyy-MM-ddTHH:mm', // Specify the date format here, matching your data
+                unit: 'day',
+                displayFormats: {
+                  day: 'yyyy-MM-dd'
                 }
-            });
+              },
+              position: 'bottom'
+            },
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            annotation: {
+              annotations: eventAnnotations
+            }
+          }
         }
-    } catch (error) {
-        console.error('Error processing data:', error);
+      });
     }
-}
+  }
 </script>
+
 <div class="container">
   <div class="main-content">
-<button class="btn btn-outline-success" on:click={fetchMoldData}>Fetch Mold Data</button>
-<canvas bind:this={chartContainer}></canvas>
-</div>
+    <button on:click={fetchMoldData}>Hämta klimatdata</button>
+    <canvas bind:this={chartContainer}></canvas>
+  </div>
 </div>
