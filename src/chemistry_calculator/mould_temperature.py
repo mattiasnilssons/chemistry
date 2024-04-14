4953b81a7feb82673ca646afd37261cb4b74a29d
mn-thermodynamics <mn@heaten.com>
2024-04-14 11:51:03 +0200

    Get python names and excel

--- src/chemistry_calculator/mould_temperature.py
+++ src/chemistry_calculator/mould_temperature.py
@@ -5,9 +5,11 @@ def get_mould_data():
     print("Current working directory:", os.getcwd())
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     pathname = os.path.join(BASE_DIR, 'temp_rh_time.xlsx')
+    print("Pathname:", pathname)
     df = pd.read_excel(pathname, header=1)
     df['Unit'] = pd.to_datetime(df['Unit'])
     df['dates'] = df['Unit']
     df["temperatures"] = df['°C']
     df["humidity"] = df['%RH']
+    print(f"Excel file: {df.head(5)}")
     return df
